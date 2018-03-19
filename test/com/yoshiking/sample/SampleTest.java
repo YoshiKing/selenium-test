0e53f550e94c5365d3b2551403921f3c791870b8
Yoshiki Yamamoto <crocodile_yoshi@yahoo.co.jp>
2018-03-19 16:41:51 +0900

    build.xmlの修正と試験実施時の画面キャプチャ取得処理を追加。

--- test/com/yoshiking/sample/SampleTest.java
+++ test/com/yoshiking/sample/SampleTest.java
@@ -1,10 +1,18 @@
 package com.yoshiking.sample;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.Augmenter;
 
 /**
  * サンプルテストクラス
@@ -39,6 +47,18 @@ public class SampleTest {
 
 		// sleep
 		Thread.sleep(5000);
+
+		// Capture
+		TakesScreenshot ts = (TakesScreenshot) new Augmenter().augment(driver);
+		try {
+			FileUtils.moveFile(ts.getScreenshotAs(OutputType.FILE), new File("./junit/img-" + System.currentTimeMillis() + ".png"));
+		} catch (WebDriverException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 
 		// exit
 		driver.quit();
